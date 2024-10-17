235ab04c2f564011ab6359a0e0e5f492842dd57c
skcbienlaogu <54233116+skcbienlaogu@users.noreply.github.com>
2024-10-17 16:10:07 +0800

    Update OddEven.java

--- src/main/java/org/example/OddEven.java
+++ src/main/java/org/example/OddEven.java
@@ -2,24 +2,38 @@ package org.example;
 
 import java.util.Scanner;
 
-public class OddEven {
+public class Main {
+
     public static void main(String[] args) {
-        int number;
-        int countOdd = 0;
-        int countEven = 0;
-        Scanner in = new Scanner(System.in); // 数字用空格或换行隔开，每次读入一个，直到输入‘-1’
-        do{
-            number = in.nextInt();
-            if(number != -1)
-            {
-                if (number % 2 == 0) {
-                    countEven += 1;
-                } else {
-                    countOdd += 1;
-                }
+        Scanner shuru = new Scanner(System.in);
+        System.out.print("请输入一个非负整数：");
+        int number = shuru.nextInt();
+        if(number>=100000||number<0){
+            System.out.println("输入错误输入范围值是(0,100000)");
+        }
+        else if(number>0){
+            int result = cal(number);
+            System.out.println(result);
+        }
+
+
+    }
+
+    public static int cal(int number) {
+        String num = String.valueOf(number);
+        int binaryValue = 0;
+
+        // 从个位开始遍历，位数从1开始
+        for (int i = 0; i < num.length(); i++) {
+            int digit = Character.getNumericValue(num.charAt(num.length() - 1 - i));
+            int position = i + 1; // 位数从1开始
+
+            // 判断数字和位数的奇偶性
+            if ((digit % 2) == (position % 2)) {
+                binaryValue += (1 << i); // 如果相同，则对应位加1
             }
-        }while(number != -1); // 理解do while和while循环的区别
-        System.out.print(countOdd+" ");
-        System.out.println(countEven);
+        }
+
+        return binaryValue;
     }
 }
