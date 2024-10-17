b37ebc434c71776f208693ff8181e85a60cd6851
skcbienlaogu <54233116+skcbienlaogu@users.noreply.github.com>
2024-10-17 16:25:24 +0800

    Update OddEven.java
    改类名

--- src/main/java/org/example/OddEven.java
+++ src/main/java/org/example/OddEven.java
@@ -2,7 +2,7 @@ package org.example;
 
 import java.util.Scanner;
 
-public class Main {
+public class OddEven {
 
     public static void main(String[] args) {
         Scanner shuru = new Scanner(System.in);
